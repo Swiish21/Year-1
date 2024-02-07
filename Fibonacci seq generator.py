18e210b98f5a9fa7690f2a32600137a52715cf52
Swiish21 <154547110+Swiish21@users.noreply.github.com>
2024-02-07 22:59:46 +0300

    Add files via upload

---
+++ Fibonacci seq generator.py
@@ -0,0 +1,10 @@
+def fibonacci_sequence(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+terms = int(input("Enter the number of Fibonacci sequence terms to generate: "))
+result = fibonacci_sequence(terms)
+
+print(f"The first {terms} terms of the Fibonacci sequence are: {result}")
