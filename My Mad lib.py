26f6245bdfc1c75aeaacf89241307c71ed6c6c5c
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-08-07 22:52:42 +0300

    Update My Mad lib.py

--- My Mad lib.py
+++ My Mad lib.py
@@ -1,7 +1,7 @@
 noun = input("Noun:")
 verb_1 = input("Verb:")
 verb_2 = input("verb:")
-adj = input("adj:")
+adj = input("adjj:")
 
 madlib = "Hello my name is Kevin, I am an " + noun + ".I love to " + verb_1 + " movies and take long walks. Last week I " + verb_2 + " to New york and saw a " + adj + "skyscrapper."
 
