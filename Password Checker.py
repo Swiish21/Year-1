dd3d7852534ba952ac9fb94c816c772f3b881899
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-08-26 14:41:57 +0300

    Update Password Checker.py

--- Password Checker.py
+++ Password Checker.py
@@ -2,7 +2,7 @@
 def verify_password(user_password,entered_password):
     return user_password == entered_password
 
-user_password = "Djmalcom19"
+user_password = "Djmalcom19."
 
 entered_password = input("Enter Password: ")
 
