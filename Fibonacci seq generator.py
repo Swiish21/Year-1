bce64ac36d0b30be35c905053842b9db5721fe53
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-10-15 13:41:33 +0300

    Update Fibonacci seq generator.py

--- Fibonacci seq generator.py
+++ Fibonacci seq generator.py
@@ -4,7 +4,7 @@ def fibonacci_sequence(n):
         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
     return fib_sequence
 
-terms = int(input("Enter the number of Fibonacci sequence terms to generate: "))
+terms = int(input("Enter the number of Fibonacci sequence to generate: "))
 result = fibonacci_sequence(terms)
 
 print(f"The first {terms} terms of the Fibonacci sequence are: {result}")
